259d19a251436b424d1fa52f58df7762ae30f8c7
Yeesian Ng <ngyeesian@gmail.com>
2015-06-01 03:03:01 -0400

    remove getGeomCoordinates

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,15 +21,15 @@ polygon = LibGEOS.createPolygon(shell,LibGEOS.GEOSGeom[hole1,hole2])
 @fact LibGEOS.geomTypeId(polygon) => LibGEOS.GEOS_POLYGON
 @fact LibGEOS.geomArea(polygon) => roughly(98.0, 1e-5)
 exterior = LibGEOS.exteriorRing(polygon)
-@fact LibGEOS.getGeomCoordinates(exterior) => roughly(Vector{Float64}[[0,0],[10,0],[10,10],[0,10],[0,0]], 1e-5)
+@fact LibGEOS.getCoordinates(LibGEOS.getCoordSeq(exterior)) => roughly(Vector{Float64}[[0,0],[10,0],[10,10],[0,10],[0,0]], 1e-5)
 interiors = LibGEOS.interiorRings(polygon)
-@fact LibGEOS.getGeomCoordinates(interiors[1]) => roughly(Vector{Float64}[[1,8],[2,8],[2,9],[1,9],[1,8]], 1e-5)
-@fact LibGEOS.getGeomCoordinates(interiors[2]) => roughly(Vector{Float64}[[8,1],[9,1],[9,2],[8,2],[8,1]], 1e-5)
+@fact LibGEOS.getCoordinates(LibGEOS.getCoordSeq(interiors[1])) => roughly(Vector{Float64}[[1,8],[2,8],[2,9],[1,9],[1,8]], 1e-5)
+@fact LibGEOS.getCoordinates(LibGEOS.getCoordSeq(interiors[2])) => roughly(Vector{Float64}[[8,1],[9,1],[9,2],[8,2],[8,1]], 1e-5)
 
 # Interpolation and Projection
 ls = LibGEOS.createLineString(Vector{Float64}[[8,1],[9,1],[9,2],[8,2]])
 pt = LibGEOS.interpolate(ls, 2.5)
-coords = LibGEOS.getGeomCoordinates(pt)
+coords = LibGEOS.getCoordinates(LibGEOS.getCoordSeq(pt))
 @fact length(coords) => 1
 @fact coords[1] => roughly([8.5, 2.0], 1e-5)
 p1 = LibGEOS.createPoint(Float64[10,1])
