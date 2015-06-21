f0d2ee99742f69dda0235702ad19f7f8d36eb6ac
Yeesian Ng <ngyeesian@gmail.com>
2015-06-21 13:56:43 -0400

    update module

--- src/LibGEOS.jl
+++ src/LibGEOS.jl
@@ -4,17 +4,12 @@ module LibGEOS
 
     using Compat, GeoInterface
 
-    export #geos_types
-            Position,
-            Point,
-            LinearRing,
-            LineString,
-            Polygon,
-
-            ndim,
-            coordinates
-
-
+    #export #geos_types
+            # Position,
+            # Point,
+            # LinearRing,
+            # LineString,
+            # Polygon
 
     include("geos_c.jl")
     
@@ -32,14 +27,8 @@ module LibGEOS
             geos_status
         end
     end
-
-    function initializeGEOS()
-        initGEOS(C_NULL, C_NULL)
-    end
-
-    function finalizeGEOS(status::GEOSconnection)
-        finishGEOS()
-    end
+    initializeGEOS() = initGEOS(C_NULL, C_NULL)
+    finalizeGEOS(status::GEOSconnection) = finishGEOS()
 
     status = GEOSconnection()
 
@@ -47,5 +36,5 @@ module LibGEOS
 
     include("geos_functions.jl")
     include("geos_types.jl")
-    include("geos_interface.jl")
+    include("geo_interface.jl")
 end
