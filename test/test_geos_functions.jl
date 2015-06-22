ecc636ecd9afd4035ec48e1823f92b5dacd9e424
Yeesian Ng <ngyeesian@gmail.com>
2015-06-21 22:51:36 -0400

    more bugfixes

--- test/test_geos_functions.jl
+++ test/test_geos_functions.jl
@@ -78,7 +78,7 @@ LibGEOS.destroyGeom(geom1_)
 LibGEOS.destroyGeom(geom2_)
 
 geom1_ = LibGEOS.geomFromWKT("POLYGON((1 1,1 5,5 5,5 1,1 1))")
-geom2_ = LibGEOS.geomFromWKT("POINT(2 2)");
+geom2_ = LibGEOS.geomFromWKT("POINT(2 2)")
 @fact LibGEOS.contains(geom1_, geom2_) => true
 @fact LibGEOS.contains(geom2_, geom1_) => false
 LibGEOS.destroyGeom(geom1_)
@@ -88,10 +88,11 @@ geom1_ = LibGEOS.geomFromWKT("MULTIPOLYGON(((0 0,0 10,10 10,10 0,0 0)))")
 geom2_ = LibGEOS.geomFromWKT("POLYGON((1 1,1 2,2 2,2 1,1 1))")
 @fact LibGEOS.contains(geom1_, geom2_) => true
 @fact LibGEOS.contains(geom2_, geom1_) => false
+LibGEOS.destroyGeom(geom1_)
+LibGEOS.destroyGeom(geom2_)
 
 # GEOSConvexHullTest
-
-input_ = LibGEOS.geomFromWKT("MULTIPOINT (130 240, 130 240, 130 240, 570 240, 570 240, 570 240, 650 240)");
+input_ = LibGEOS.geomFromWKT("MULTIPOINT (130 240, 130 240, 130 240, 570 240, 570 240, 570 240, 650 240)")
 expected_ = LibGEOS.geomFromWKT("LINESTRING (130 240, 650 240)")
 output_ = LibGEOS.convexhull(input_)
 @fact LibGEOS.isEmpty(output_) => false
@@ -144,17 +145,19 @@ LibGEOS.setZ!(cs_, 1, z)
 @fact LibGEOS.getZ(cs_, 1) => roughly(z, 1e-5)
 
 # LibGEOS.delaunayTriangulationTest
-geom1_ = LibGEOS.geomFromWKT("POLYGON EMPTY");
+geom1_ = LibGEOS.geomFromWKT("POLYGON EMPTY")
 @fact LibGEOS.isEmpty(geom1_) => true
 geom2_ = LibGEOS.delaunayTriangulation(geom1_,0.0,true)
 @fact LibGEOS.isEmpty(geom2_) => true
 @fact LibGEOS.geomTypeId(geom2_) => LibGEOS.GEOS_MULTILINESTRING
+LibGEOS.destroyGeom(geom1_)
 LibGEOS.destroyGeom(geom2_)
 
-geom1_ = LibGEOS.geomFromWKT("POINT(0 0)");
+geom1_ = LibGEOS.geomFromWKT("POINT(0 0)")
 geom2_ = LibGEOS.delaunayTriangulation(geom1_, 0.0, false)
 @fact LibGEOS.isEmpty(geom2_) => true
 @fact LibGEOS.geomTypeId(geom2_) => LibGEOS.GEOS_GEOMETRYCOLLECTION
+LibGEOS.destroyGeom(geom1_)
 LibGEOS.destroyGeom(geom2_)
 
 geom1_ = LibGEOS.geomFromWKT("MULTIPOINT(0 0, 5 0, 10 0)")
@@ -181,21 +184,31 @@ LibGEOS.destroyGeom(geom3_)
 geom1_ = LibGEOS.geomFromWKT("POINT(10 10)")
 geom2_ = LibGEOS.geomFromWKT("POINT(3 6)")
 @fact LibGEOS.geomDistance(geom1_, geom2_) => roughly(8.06225774829855, 1e-12)
+LibGEOS.destroyGeom(geom1_)
+LibGEOS.destroyGeom(geom2_)
 
 # GEOSGeom_extractUniquePointsTest
 geom1_ = LibGEOS.geomFromWKT("POLYGON EMPTY")
-geom3_ = LibGEOS.uniquePoints(geom1_)
-@fact LibGEOS.isEmpty(geom3_) => true
+geom2_ = LibGEOS.uniquePoints(geom1_)
+@fact LibGEOS.isEmpty(geom2_) => true
+LibGEOS.destroyGeom(geom1_)
+LibGEOS.destroyGeom(geom2_)
 
 geom1_ = LibGEOS.geomFromWKT("MULTIPOINT(0 0, 0 0, 1 1)")
 geom2_ = LibGEOS.geomFromWKT("MULTIPOINT(0 0, 1 1)")
 geom3_ = LibGEOS.uniquePoints(geom1_)
 @fact LibGEOS.equals(geom3_, geom2_) => true
+LibGEOS.destroyGeom(geom1_)
+LibGEOS.destroyGeom(geom2_)
+LibGEOS.destroyGeom(geom3_)
 
 geom1_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION(MULTIPOINT(0 0, 0 0, 1 1),LINESTRING(1 1, 2 2, 2 2, 0 0),POLYGON((5 5, 0 0, 0 2, 2 2, 5 5)))")
 geom2_ = LibGEOS.geomFromWKT("MULTIPOINT(0 0, 1 1, 2 2, 5 5, 0 2)")
 geom3_ = LibGEOS.uniquePoints(geom1_)
 @fact LibGEOS.equals(geom3_, geom2_) => true
+LibGEOS.destroyGeom(geom1_)
+LibGEOS.destroyGeom(geom2_)
+LibGEOS.destroyGeom(geom3_)
 
 # GEOSGetCentroidTest
 geom1_ = LibGEOS.geomFromWKT("POINT(10 0)")
@@ -285,6 +298,8 @@ geom1_ = LibGEOS.geomFromWKT("MULTIPOLYGON(((0 0,0 10,10 10,10 0,0 0)))")
 geom2_ = LibGEOS.geomFromWKT("POLYGON((1 1,1 2,2 2,2 1,1 1))")
 @fact LibGEOS.intersects(geom1_, geom2_) => true
 @fact LibGEOS.intersects(geom2_, geom1_) => true
+LibGEOS.destroyGeom(geom1_)
+LibGEOS.destroyGeom(geom2_)
 
 # LineString_PointTest
 geom1 = LibGEOS.geomFromWKT("LINESTRING(0 0, 5 5, 10 10)")
@@ -331,7 +346,7 @@ LibGEOS.destroyCoordSeq(coords_)
 # GEOSNodeTest
 geom1_ = LibGEOS.geomFromWKT("LINESTRING(0 0, 10 10, 10 0, 0 10)")
 geom2_ = LibGEOS.node(geom1_)
-LibGEOS.normalize(geom2_)
+LibGEOS.normalize!(geom2_)
 geom3_ = LibGEOS.geomFromWKT("MULTILINESTRING ((5 5, 10 0, 10 10, 5 5), (0 10, 5 5), (0 0, 5 5))")
 @fact LibGEOS.geomToWKT(geom2_) => LibGEOS.geomToWKT(geom3_)
 LibGEOS.destroyGeom(geom1_)
@@ -340,7 +355,7 @@ LibGEOS.destroyGeom(geom3_)
 
 geom1_ = LibGEOS.geomFromWKT("MULTILINESTRING((0 0, 2 0, 4 0),(5 0, 3 0, 1 0))")
 geom2_ = LibGEOS.node(geom1_)
-LibGEOS.normalize(geom2_)
+LibGEOS.normalize!(geom2_)
 geom3_ = LibGEOS.geomFromWKT("MULTILINESTRING ((4 0, 5 0), (3 0, 4 0), (2 0, 3 0), (1 0, 2 0), (0 0, 1 0))")
 @fact LibGEOS.geomToWKT(geom2_) => LibGEOS.geomToWKT(geom3_)
 LibGEOS.destroyGeom(geom1_)
@@ -349,7 +364,7 @@ LibGEOS.destroyGeom(geom3_)
 
 geom1_ = LibGEOS.geomFromWKT("MULTILINESTRING((0 0, 2 0, 4 0),(0 0, 2 0, 4 0))")
 geom2_ = LibGEOS.node(geom1_)
-LibGEOS.normalize(geom2_)
+LibGEOS.normalize!(geom2_)
 geom3_ = LibGEOS.geomFromWKT("MULTILINESTRING ((2 0, 4 0), (0 0, 2 0))")
 @fact LibGEOS.geomToWKT(geom2_) => LibGEOS.geomToWKT(geom3_)
 LibGEOS.destroyGeom(geom1_)
@@ -465,18 +480,18 @@ LibGEOS.destroyGeom(geom4_)
 # /// Illegal case (point-poly)
 # void object::test<1>()
 # {
-#     geom1_ = GEOSGeomFromWKT("POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))");
-#     geom2_ = GEOSGeomFromWKT("POINT(0.5 0)");
-#     geom3_ = GEOSSharedPaths(geom1_, geom2_);
+#     geom1_ = GEOSGeomFromWKT("POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))")
+#     geom2_ = GEOSGeomFromWKT("POINT(0.5 0)")
+#     geom3_ = GEOSSharedPaths(geom1_, geom2_)
 
-#     ensure(!geom3_);
+#     ensure(!geom3_)
 # }
 
 # http://trac.osgeo.org/postgis/ticket/670#comment:3
 # geom1_ = LibGEOS.geomFromWKT("POINT(-11.1111111 40)")
 # geom2_ = LibGEOS.geomFromWKT("POLYGON((-8.1111111 60,-8.16875525879031 59.4147290339516,-8.33947250246614 58.8519497029047,-8.61670226309236 58.3332893009412,-8.98979075644036 57.8786796564404,-9.44440040094119 57.5055911630924,-9.96306080290473 57.2283614024661,-10.5258401339516 57.0576441587903,-11.1111111 57,-11.6963820660484 57.0576441587903,-12.2591613970953 57.2283614024661,-12.7778217990588 57.5055911630924,-13.2324314435596 57.8786796564404,-13.6055199369076 58.3332893009412,-13.8827496975339 58.8519497029047,-14.0534669412097 59.4147290339516,-14.1111111 60,-14.0534669412097 60.5852709660484,-13.8827496975339 61.1480502970953,-13.6055199369076 61.6667106990588,-13.2324314435597 62.1213203435596,-12.7778217990588 62.4944088369076,-12.2591613970953 62.7716385975339,-11.6963820660484 62.9423558412097,-11.1111111 63,-10.5258401339516 62.9423558412097,-9.96306080290474 62.7716385975339,-9.4444004009412 62.4944088369076,-8.98979075644036 62.1213203435596,-8.61670226309237 61.6667106990588,-8.33947250246614 61.1480502970953,-8.16875525879031 60.5852709660484,-8.1111111 60))")
 # geom3_ = LibGEOS.sharedPaths(geom1_, geom2_)
-# ensure(!geom3_);
+# ensure(!geom3_)
 
 # GEOSSimplifyTest
 geom1_ = LibGEOS.geomFromWKT("POLYGON EMPTY")
@@ -607,7 +622,7 @@ LibGEOS.destroyGeom(geom1_)
 LibGEOS.destroyGeom(geom2_)
 LibGEOS.destroyGeom(geom3_)
 
-geom1_ = LibGEOS.geomFromWKT("MULTIPOINT (4 5, 6 7, 4 5, 6 5, 6 7)");
+geom1_ = LibGEOS.geomFromWKT("MULTIPOINT (4 5, 6 7, 4 5, 6 5, 6 7)")
 geom2_ = LibGEOS.unaryUnion(geom1_)
 geom3_ = LibGEOS.geomFromWKT("MULTIPOINT (4 5, 6 5, 6 7)")
 @fact LibGEOS.geomToWKT(geom2_) => LibGEOS.geomToWKT(geom3_)
@@ -615,32 +630,32 @@ LibGEOS.destroyGeom(geom1_)
 LibGEOS.destroyGeom(geom2_)
 LibGEOS.destroyGeom(geom3_)
 
-geom1_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION (POINT(4 5), MULTIPOINT(6 7, 6 5, 6 7), LINESTRING(0 5, 10 5), LINESTRING(4 -10, 4 10))");
-geom2_ = LibGEOS.unaryUnion(geom1_);
+geom1_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION (POINT(4 5), MULTIPOINT(6 7, 6 5, 6 7), LINESTRING(0 5, 10 5), LINESTRING(4 -10, 4 10))")
+geom2_ = LibGEOS.unaryUnion(geom1_)
 geom3_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION (POINT (6 7), LINESTRING (4 -10, 4 5), LINESTRING (4 5, 4 10), LINESTRING (0 5, 4 5), LINESTRING (4 5, 10 5))")
 @fact LibGEOS.geomToWKT(geom2_) => LibGEOS.geomToWKT(geom3_)
 LibGEOS.destroyGeom(geom1_)
 LibGEOS.destroyGeom(geom2_)
 LibGEOS.destroyGeom(geom3_)
 
-geom1_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION (POINT(4 5), MULTIPOINT(6 7, 6 5, 6 7), POLYGON((0 0, 10 0, 10 10, 0 10, 0 0),(5 6, 7 6, 7 8, 5 8, 5 6)))");
-geom2_ = LibGEOS.unaryUnion(geom1_);
+geom1_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION (POINT(4 5), MULTIPOINT(6 7, 6 5, 6 7), POLYGON((0 0, 10 0, 10 10, 0 10, 0 0),(5 6, 7 6, 7 8, 5 8, 5 6)))")
+geom2_ = LibGEOS.unaryUnion(geom1_)
 geom3_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION (POINT (6 7), POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0), (5 6, 7 6, 7 8, 5 8, 5 6)))")
 @fact LibGEOS.geomToWKT(geom2_) => LibGEOS.geomToWKT(geom3_)
 LibGEOS.destroyGeom(geom1_)
 LibGEOS.destroyGeom(geom2_)
 LibGEOS.destroyGeom(geom3_)
 
-geom1_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION (MULTILINESTRING((5 7, 12 7), (4 5, 6 5), (5.5 7.5, 6.5 7.5)), POLYGON((0 0, 10 0, 10 10, 0 10, 0 0),(5 6, 7 6, 7 8, 5 8, 5 6)))");
-geom2_ = LibGEOS.unaryUnion(geom1_);
+geom1_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION (MULTILINESTRING((5 7, 12 7), (4 5, 6 5), (5.5 7.5, 6.5 7.5)), POLYGON((0 0, 10 0, 10 10, 0 10, 0 0),(5 6, 7 6, 7 8, 5 8, 5 6)))")
+geom2_ = LibGEOS.unaryUnion(geom1_)
 geom3_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION (LINESTRING (5 7, 7 7), LINESTRING (10 7, 12 7), LINESTRING (5.5 7.5, 6.5 7.5), POLYGON ((10 7, 10 0, 0 0, 0 10, 10 10, 10 7), (5 6, 7 6, 7 7, 7 8, 5 8, 5 7, 5 6)))")
 @fact LibGEOS.geomToWKT(geom2_) => LibGEOS.geomToWKT(geom3_)
 LibGEOS.destroyGeom(geom1_)
 LibGEOS.destroyGeom(geom2_)
 LibGEOS.destroyGeom(geom3_)
 
-geom1_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION (MULTILINESTRING((5 7, 12 7), (4 5, 6 5), (5.5 7.5, 6.5 7.5)), POLYGON((0 0, 10 0, 10 10, 0 10, 0 0),(5 6, 7 6, 7 8, 5 8, 5 6)), MULTIPOINT(6 6.5, 6 1, 12 2, 6 1))");
-geom2_ = LibGEOS.unaryUnion(geom1_);
+geom1_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION (MULTILINESTRING((5 7, 12 7), (4 5, 6 5), (5.5 7.5, 6.5 7.5)), POLYGON((0 0, 10 0, 10 10, 0 10, 0 0),(5 6, 7 6, 7 8, 5 8, 5 6)), MULTIPOINT(6 6.5, 6 1, 12 2, 6 1))")
+geom2_ = LibGEOS.unaryUnion(geom1_)
 geom3_ = LibGEOS.geomFromWKT("GEOMETRYCOLLECTION (POINT (6 6.5), POINT (12 2), LINESTRING (5 7, 7 7), LINESTRING (10 7, 12 7), LINESTRING (5.5 7.5, 6.5 7.5), POLYGON ((10 7, 10 0, 0 0, 0 10, 10 10, 10 7), (5 6, 7 6, 7 7, 7 8, 5 8, 5 7, 5 6)))")
 @fact LibGEOS.geomToWKT(geom2_) => LibGEOS.geomToWKT(geom3_)
 LibGEOS.destroyGeom(geom1_)
