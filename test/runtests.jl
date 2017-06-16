c5ad373af2864c78b7ba28d5e3161519dd46ce2e
Maarten Pronk <git@evetion.nl>
2017-06-16 12:44:30 +0200

    Added regression testing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,6 @@ using FactCheck, LibGEOS
 include("test_geos_functions.jl")
 include("test_geos_operations.jl")
 include("test_geo_interface.jl")
+include("test_regressions.jl")
 
 FactCheck.exitstatus()
