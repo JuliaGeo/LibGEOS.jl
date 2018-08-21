f2bd58fed9c5656d705ada2a94926e37902b4633
Martijn Visser <mgvisser@gmail.com>
2018-08-21 22:53:55 +0200

    try enabling test that crashed GEOS before

--- test/test_invalid_geometry.jl
+++ test/test_invalid_geometry.jl
@@ -1,15 +1,12 @@
 @testset "LibGEOS invalid geometry" begin
-    # LibGEOS shouldn't crash but error out
-    # on invalid geometry
+    # LibGEOS shouldn't crash but error out on invalid geometry
 
     # Self intersecting polygon
-    # gives unkown function segfault on Travis Linux CI, see #44, revisit after #21
-    # polygon = LibGEOS._readgeom("POLYGON((0 0, 10 10, 0 10, 10 0, 0 0))")
-    # @test !LibGEOS.isValid(polygon)
+    polygon = LibGEOS._readgeom("POLYGON((0 0, 10 10, 0 10, 10 0, 0 0))")
+    @test !LibGEOS.isValid(polygon)
 
     # Hole outside of base
-    # gives unkown function segfault on Travis Linux CI, see #44, revisit after #21
-    # polygon = LibGEOS._readgeom("POLYGON((0 0, 10 0, 10 10, 0 10, 0 0), (15 15, 15 20, 20 20, 20 15, 15 15))")
-    # @test !LibGEOS.isValid(polygon)
-
+    polygon = LibGEOS._readgeom("POLYGON((0 0, 10 0, 10 10, 0 10, 0 0),
+        (15 15, 15 20, 20 20, 20 15, 15 15))")
+    @test !LibGEOS.isValid(polygon)
 end
