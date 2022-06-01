841339d3ee04330cbc7673a29a544ea42f2e877b
Martijn Visser <mgvisser@gmail.com>
2022-06-01 21:05:33 +0200

    wrap GEOS 3.10.2
    And add warning/reminder to tests to regenerate
    bindings if the versions don't match.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,14 @@ using Test, LibGEOS
 version = LibGEOS.GEOSversion()
 @info "GEOS version $version"
 
+if version != LibGEOS.GEOS_CAPI_VERSION
+    @warn string(
+        "The GEOS version in use is different from the version used to ",
+        "generate libgeos_api.jl. Consider regenerating if the difference is more than a ",
+        "patch release.",
+    ) run_version = version gen_version = LibGEOS.GEOS_CAPI_VERSION
+end
+
 @testset "LibGEOS" begin
     include("test_geos_functions.jl")
     include("test_geos_operations.jl")
